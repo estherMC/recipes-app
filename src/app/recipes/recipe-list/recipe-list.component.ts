93958068934a81c707138304dadbdf1d826c1f42
Esther Martorell <esther.martorell@allianz.es>
2023-02-12 08:17:56 +0100

    :sparkles: Add first version of recipes list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+
+export class RecipeListComponent implements OnInit  {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply test', 'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
+  ];
+
+  constructor(){}
+
+  ngOnInit() {
+      
+  }
 
 }
