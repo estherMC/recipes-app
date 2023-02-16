62c36628d5c10db706a17dde95c807b26ca5c414
Esther Martorell <esther.martorell@allianz.es>
2023-02-16 07:59:16 +0100

    :sparkles: Add alternative constructor of ingredient simpliest

--- src/app/shared/ingredient.model.ts
+++ src/app/shared/ingredient.model.ts
@@ -1,9 +1,12 @@
 export class Ingredient {
-    public name:  string;
-    public amount: number;
+    // public name:  string;
+    // public amount: number;
 
-    constructor(name: string, amount: number){
-        this.name = name;
-        this.amount = amount;
-    }
+    // constructor(name: string, amount: number){
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    //Same logic than above (like lombok)
+    constructor(public name: string, public amount: number){}
 }
